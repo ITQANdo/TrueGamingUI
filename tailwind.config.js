744aa89446032281a92430201f7165a8999c6d71
Jaber Zarif <jaber@itqan.do>
2019-08-27 13:40:30 +0100

    Truegaming UI first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        navbg: 'rgba(61,66,71,.8)',
+        sectionwhite: '#F0F0F0',
+        sectiongray: '#E5E5E5'
+      },
+      lineHeight: {
+        3: 3,
+      },
+      fontFamily: {
+        'araby-regular': ['AlarabyTelevision-Regular','-apple-system', 'BlinkMacSystemFont'],
+        'araby-medium': ['AlarabyTelevision-Medium','-apple-system', 'BlinkMacSystemFont'],
+        'araby-bold': ['AlarabyTelevision-Bold','-apple-system', 'BlinkMacSystemFont']
+      },
+      height: {
+        siema: '450px'
+      },
+      width: {
+        articles: '770px',
+        sidebar: '340px',
+      },
+      borderRadius: {
+        'card':  '17px 17px 0'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+
+}
