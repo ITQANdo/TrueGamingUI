9036acc6ee87bce484c826e039bbe92d96a161b8
jaberzarif <jaber.zarif@outlook.fr>
2019-08-29 13:16:49 +0100

    added Responsive view

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,27 @@
 const mySiema = new Siema({
-    selector: '.siema',
-    duration: 200,
-    easing: 'ease-out',
-    perPage: 1,
-    startIndex: 0,
-    draggable: true,
-    multipleDrag: true,
-    threshold: 20,
-    loop: true,
-    rtl: true,
-    onInit: () => {},
-    onChange: () => {},
-  });
+  selector: '.siema',
+  duration: 200,
+  easing: 'ease-out',
+  perPage: 1,
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: true,
+  rtl: true,
+  onInit: () => {},
+  onChange: () => {},
+});
 //document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
 //document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
+function toggleMenu() {
+  console.log("hello");
+
+  var menuMobile = document.querySelector(".show-menu");
+  if (menuMobile.style.display == "block") {
+    menuMobile.style.display = "none";
+  } else {
+    menuMobile.style.display = "block";
+  }
+}
